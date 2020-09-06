56f79750d341f96f702f96712d5fe0673cea0585
cycloneboy <xuanfeng1992@gmail.com>
2020-09-06 15:22:43 +0800

    添加使用net的mnist手写数字识别

--- deep/torch/mnist_first.py
+++ deep/torch/mnist_first.py
@@ -1,45 +1,49 @@
 #!/user/bin/env python
 # -*- coding: utf-8 -*-
-# @File  : mnist_firtst.py
+# @File  : mnist_first.py
 # @Author: sl
 # @Date  : 2020/9/5 - 下午10:45
 
-import torchvision.datasets
-import torchvision.transforms
-import torch.utils.data
 import matplotlib.pyplot as plt
-import torch.nn
-import torch.optim
 import torch
+import torch.utils.data
+import torchvision.transforms
 
 
 # 加载训练集
 train_dataset = torchvision.datasets.MNIST(root='~/workspace/data/mnist',
-                                           train=True, transform=torchvision.transforms.ToTensor,
+                                           train=True, transform=torchvision.transforms.ToTensor(),
                                            download=True)
 test_dataset = torchvision.datasets.MNIST(root='~/workspace/data/mnist',
-                                           train=False, transform=torchvision.transforms.ToTensor,
-                                           download=True)
-
+                                          train=False, transform=torchvision.transforms.ToTensor(),
+                                          download=True)
 
 # 转载数据
 batch_size = 100
-train_loader =torch.utils.data.DataLoader(dataset=train_dataset,batch_size=batch_size)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size)
 print('len(train_loader) = {}'.format(len(train_dataset)))
 
-test_loader =torch.utils.data.DataLoader(dataset=test_dataset,batch_size=batch_size)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size)
 print('len(test_loader) = {}'.format(len(test_loader)))
+
+# 显示图像１
+images, label = next(iter(train_loader))
+images_example = torchvision.utils.make_grid(images)
+images_example = images_example.numpy().transpose(1, 2, 0)  # 将图像的通道值置换到最后的维度，符合图像的格式
+mean = [0.5, 0.5, 0.5]
+std = [0.5, 0.5, 0.5]
+images_example = images_example * std + mean
+plt.imshow(images_example)
+plt.show()
 
 for images, labels in train_loader:
     print('image.size() = {}'.format(images.size()))
     print('labels.size() = {}'.format(labels.size()))
     break
 
-
-# 显示图像
-plt.imshow(images[0,0],cmap='gray')
+# 显示图像２
+plt.imshow(images[0, 0], cmap='gray')
 plt.title("label = {}".format(labels[0]))
-
 
 # 训练线性分类器
 
@@ -50,18 +54,17 @@ optimizer = torch.optim.Adam(fc.parameters())
 # 训练数据
 num_epochs = 5
 for epoch in range(num_epochs):
-    for idx, (images,labels) in enumerate(train_loader):
-        x = images.reshape(-1,28 * 28)
+    for idx, (images, labels) in enumerate(train_loader):
+        x = images.reshape(-1, 28 * 28)
 
         optimizer.zero_grad()
         preds = fc(x)
-        loss = criterion(preds,labels)
+        loss = criterion(preds, labels)
         loss.backward()
         optimizer.step()
 
         if idx % 100 == 0:
             print('第{}趟第{}批: loss = {:g}'.format(epoch, idx, loss))
-
 
 # 测试数据
 correct = 0
@@ -69,28 +72,10 @@ total = 0
 for images, labels in test_loader:
     x = images.reshape(-1, 28 * 28)
     preds = fc(x)
-    predicted = torch.argmax(preds,1)
+    predicted = torch.argmax(preds, 1)
     total += labels.size(0)
     correct += (predicted == labels).sum().item()
 
 accuracy = correct / total
 print('测试集上的准确率:{}'.format(accuracy))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# 测试集上的准确率:0.9221
---
+++ deep/torch/mnist_nn.py
@@ -0,0 +1,148 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : mnist_nn.py
+# @Author: sl
+# @Date  : 2020/9/6 - 下午1:41
+
+import matplotlib.pyplot as plt
+import torch
+import torch.utils.data
+import torchvision.transforms
+import torch.optim
+import torchvision.datasets
+import torch.nn as nn
+
+import time
+import os
+
+from torch.nn.modules import Module, Conv2d, ReLU, Sequential, CrossEntropyLoss
+
+
+# 创建目录
+def mkdir(data_path):
+    if not os.path.exists(data_path):
+        os.mkdir(data_path)
+
+
+# 加载训练集
+DATA_MNIST_DIR = '~/workspace/data/mnist'
+MODEL_MNIST_DIR = '{}/model'.format(DATA_MNIST_DIR)
+
+mkdir(DATA_MNIST_DIR)
+mkdir(MODEL_MNIST_DIR)
+
+train_dataset = torchvision.datasets.MNIST(root=('%s' % DATA_MNIST_DIR),
+                                           train=True, transform=torchvision.transforms.ToTensor(),
+                                           download=True)
+test_dataset = torchvision.datasets.MNIST(root=DATA_MNIST_DIR,
+                                          train=False, transform=torchvision.transforms.ToTensor(),
+                                          download=True)
+
+# 转载数据
+batch_size = 100
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size)
+print('len(train_loader) = {}'.format(len(train_dataset)))
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size)
+print('len(test_loader) = {}'.format(len(test_loader)))
+
+
+class Net(torch.nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv0 = torch.nn.Conv2d(1, 64, kernel_size=3, padding=1)
+        self.relu1 = torch.nn.ReLU()
+        self.conv2 = torch.nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.relu3 = torch.nn.ReLU()
+        self.pool4 = torch.nn.MaxPool2d(stride=2, kernel_size=2)
+        self.fc5 = torch.nn.Linear(128 * 14 * 14, 1024)
+        self.relu6 = torch.nn.ReLU()
+        self.drop7 = torch.nn.Dropout(p=0.5)
+        self.fc8 = torch.nn.Linear(1024, 10)
+
+    def forward(self, x):
+        x = self.conv0(x)
+        x = self.relu1(x)
+        x = self.conv2(x)
+        x = self.relu3(x)
+        x = self.pool4(x)
+        x = x.view(-1, 128 * 14 * 14)
+        x = self.fc5(x)
+        x = self.relu6(x)
+        x = self.drop7(x)
+        x = self.fc8(x)
+        return x
+
+
+net = Net()
+print(net)
+
+
+class Net2(torch.nn.Module):
+
+    def __init__(self):
+        super(Net2, self).__init__()
+        self.conv = Sequential(
+            torch.nn.Conv2d(1, 64, kernel_size=3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(stride=2, kernel_size=2))
+        self.dense = Sequential(
+            torch.nn.Linear(128 * 14 * 14, 1024),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(p=0.5),
+            torch.nn.Linear(1024, 10)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = x.view(-1, 128 * 14 * 14)
+        x = self.dense(x)
+        return x
+
+
+net2 = Net2()
+print(net2)
+
+criterion = CrossEntropyLoss()
+optimizer = torch.optim.Adam(net.parameters())
+
+begin_time = time.time()
+# 2020/9/6 14:19:33
+print('开始训练时间: {}'.format(str(begin_time)))
+
+# 训练
+num_epochs = 5
+for epoch in range(num_epochs):
+    for idx, (images, labels) in enumerate(train_loader):
+        optimizer.zero_grad()
+        preds = net(images)
+        loss = criterion(preds, labels)
+        loss.backward()
+        optimizer.step()
+
+        if idx % 100 == 0:
+            print('epoch {}, batch {},loss = {:g}'.format(epoch, idx, loss.item()))
+
+end_time = time.time()
+print('结束训练时间: {}'.format(str(end_time)))
+print('训练花费的时间:{} 分钟'.format((end_time - begin_time) / 60))
+
+# 测试
+correct = 0
+total = 0
+for images, labels in test_loader:
+    preds = net(images)
+    predicted = torch.argmax(preds, 1)
+    total += labels.size(0)
+    correct += (predicted == labels).sum().item()
+
+accuracy = correct / total
+print('测试数据准确率:{:.1%}'.format(accuracy))
+
+# 保存模型
+torch.save(net, MODEL_MNIST_DIR)
+
+# 测试数据准确率:98.7%
