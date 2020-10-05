cd2499d5e48793c7f54a3eb3d21c360a41b19a2e
cycloneboy <xuanfeng1992@gmail.com>
2020-10-05 22:32:22 +0800

    添加　nlp

---
+++ deep/torch/rnn-learn.py
@@ -0,0 +1,19 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : rnn-learn.py
+# @Author: sl
+# @Date  : 2020/10/5 - 下午10:18
+
+
+import torch
+
+
+X,W_xh = torch.randn(3,1),torch.randn(1,4)
+H,H_hh = torch.randn(3,4),torch.randn(4,4)
+
+res = torch.matmul(X,W_xh) + torch.matmul(H,H_hh)
+print(res)
+
+ret = torch.matmul(torch.cat((X,H),dim=1),torch.cat((W_xh,H_hh),dim=0))
+
+print(res)
