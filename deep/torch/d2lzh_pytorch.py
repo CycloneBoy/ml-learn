bb0c48ab24345f6b83a696d4fe571a1548ecffb9
cycloneboy <xuanfeng1992@gmail.com>
2020-10-08 21:31:11 +0800

    添加　自定义循环神经网络生成歌词

--- deep/torch/d2lzh_pytorch.py
+++ deep/torch/d2lzh_pytorch.py
@@ -3,8 +3,8 @@
 # @File  : d2lzh_pytorch.py
 # @Author: sl
 # @Date  : 2020/10/4 - 上午9:58
-
-
+import math
+import random
 import sys
 import time
 import zipfile
@@ -12,6 +12,8 @@ import zipfile
 from util.constant import DATA_MNIST_DIR
 
 sys.path.append("..")
+
+import numpy as np
 
 import torch
 from torch import nn
@@ -698,6 +700,7 @@ def read_jay(length=10000):
     return corpus_chars, corpus_chars[:length]
 
 
+# 读入周杰伦的歌词
 def load_data_jay_lyrics():
     all_chars, corpus_chars = read_jay()
     idx_to_char = list(set(corpus_chars))
@@ -709,6 +712,162 @@ def load_data_jay_lyrics():
     # print('chars:', ''.join([idx_to_char[idx] for idx in sample]))
     # print('indices:', sample)
     return corpus_indices, char_to_idx, idx_to_char, vocab_size
+
+
+def data_iter_random(corpus_indices, batch_size, num_steps, device=None):
+    # 减1是因为输出的索引x是相应输入的索引y加1
+    num_examples = (len(corpus_indices) - 1) // num_steps
+    epoch_size = num_examples // batch_size
+    example_indices = list(range(num_examples))
+    random.shuffle(example_indices)
+
+    # 返回从pos开始的长为num_steps的序列
+    def _data(pos):
+        return corpus_indices[pos:pos + num_steps]
+
+    if device is None:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    for i in range(epoch_size):
+        # 每次读取batch_size个随机样本
+        i = i * batch_size
+        batch_indices = example_indices[i:i + batch_size]
+        X = [_data(j * num_steps) for j in batch_indices]
+        Y = [_data(j * num_steps + 1) for j in batch_indices]
+        yield torch.tensor(X, dtype=torch.float32, device=device), torch.tensor(Y, dtype=torch.float32, device=device)
+
+
+def test_seq():
+    my_seq = list(range(30))
+    for X, Y in data_iter_random(my_seq, batch_size=2, num_steps=6):
+        print('X: ', X, '\nY: ', Y, '\n')
+
+
+# 相邻采样
+def data_iter_consecutive(corpus_indices, batch_size, num_steps, device=None):
+    if device is None:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    corpus_indices = torch.tensor(corpus_indices, dtype=torch.float32, device=device)
+    # 减1是因为输出的索引x是相应输入的索引y加1
+    data_len = len(corpus_indices)
+    batch_len = data_len // batch_size
+
+    indices = corpus_indices[0:batch_size * batch_len].view(batch_size, batch_len)
+    epoch_size = (batch_len - 1) // num_steps
+
+    for i in range(epoch_size):
+        # 每次读取batch_size个随机样本
+        i = i * num_steps
+        X = indices[:, i:i + num_steps]
+        Y = indices[:, i + 1:i + num_steps + 1]
+        yield X, Y
+
+
+def test_seq２():
+    my_seq = list(range(30))
+    for X, Y in data_iter_consecutive(my_seq, batch_size=2, num_steps=6):
+        print('X: ', X, '\nY: ', Y, '\n')
+
+
+def one_hot(x, n_class, dtype=torch.float32):
+    # X shape: (batch), output shape: (batch, n_class)
+    x = x.long()
+    res = torch.zeros(x.shape[0], n_class, dtype=dtype, device=x.device)
+    res.scatter_(1, x.view(-1, 1), 1)
+    return res
+
+
+def to_onehot(X, n_class):
+    # X shape: (batch, seq_len), output: seq_len elements of (batch, n_class)
+    return [one_hot(X[:, i], n_class) for i in range(X.shape[1])]
+
+
+
+
+def grad_clipping(params,theta,device):
+    norm = torch.tensor([0.0],device=device)
+    for param in params:
+        norm += (params.grad.data ** 2).sum()
+    norm = norm.sqrt().item()
+    if norm > theta:
+        for param in params:
+            param.grad.data *= (theta/norm)
+
+def predict_rnn(prefix,num_chars,rnn,params,init_rnn_state,
+                num_hiddens,vocab_size,device,idx_to_char,char_to_idx):
+    state = init_rnn_state(1,num_hiddens,device)
+    output = [char_to_idx[prefix[0]]]
+
+    for t in range(num_chars + len(prefix) - 1):
+        # 将上一时间步的输出作为当前时间步的输入
+        X = to_onehot(torch.tensor([[output[-1]]],device=device),vocab_size)
+        # 计算输出和更新隐藏状态
+        (Y,state) = rnn(X,state,params)
+        # 下一个时间步的输入是prefix里的字符或者当前的最佳预测字符
+        if t < len(prefix) -1 :
+            output.append(char_to_idx[prefix[t+1]])
+        else:
+            output.append(int(Y[0].argmax(dim=1).item()))
+    return ''.join([idx_to_char[i] for i in output])
+
+
+
+# 本函数已保存在d2lzh_pytorch包中方便以后使用
+def train_and_predict_rnn(rnn, get_params, init_rnn_state, num_hiddens,
+                          vocab_size, device, corpus_indices, idx_to_char,
+                          char_to_idx, is_random_iter, num_epochs, num_steps,
+                          lr, clipping_theta, batch_size, pred_period,
+                          pred_len, prefixes):
+    if is_random_iter:
+        data_iter_fn = data_iter_random
+    else:
+        data_iter_fn = data_iter_consecutive
+    params = get_params()
+    loss = nn.CrossEntropyLoss()
+
+    for epoch in range(num_epochs):
+        if not is_random_iter:  # 如使用相邻采样，在epoch开始时初始化隐藏状态
+            state = init_rnn_state(batch_size, num_hiddens, device)
+        l_sum, n, start = 0.0, 0, time.time()
+        data_iter = data_iter_fn(corpus_indices, batch_size, num_steps, device)
+        for X, Y in data_iter:
+            if is_random_iter:  # 如使用随机采样，在每个小批量更新前初始化隐藏状态
+                state = init_rnn_state(batch_size, num_hiddens, device)
+            else:
+            # 否则需要使用detach函数从计算图分离隐藏状态, 这是为了
+            # 使模型参数的梯度计算只依赖一次迭代读取的小批量序列(防止梯度计算开销太大)
+                for s in state:
+                    s.detach_()
+
+            inputs = to_onehot(X, vocab_size)
+            # outputs有num_steps个形状为(batch_size, vocab_size)的矩阵
+            (outputs, state) = rnn(inputs, state, params)
+            # 拼接之后形状为(num_steps * batch_size, vocab_size)
+            outputs = torch.cat(outputs, dim=0)
+            # Y的形状是(batch_size, num_steps)，转置后再变成长度为
+            # batch * num_steps 的向量，这样跟输出的行一一对应
+            y = torch.transpose(Y, 0, 1).contiguous().view(-1)
+            # 使用交叉熵损失计算平均分类误差
+            l = loss(outputs, y.long())
+
+            # 梯度清0
+            if params[0].grad is not None:
+                for param in params:
+                    param.grad.data.zero_()
+            l.backward()
+            grad_clipping(params, clipping_theta, device)  # 裁剪梯度
+            # todo: 导入SGD 算法进行优化
+            # d2l.sgd(params, lr, 1)  # 因为误差已经取过均值，梯度不用再做平均
+            l_sum += l.item() * y.shape[0]
+            n += y.shape[0]
+
+        if (epoch + 1) % pred_period == 0:
+            print('epoch %d, perplexity %f, time %.2f sec' % (
+                epoch + 1, math.exp(l_sum / n), time.time() - start))
+            for prefix in prefixes:
+                print(' -', predict_rnn(prefix, pred_len, rnn, params, init_rnn_state,
+                    num_hiddens, vocab_size, device, idx_to_char, char_to_idx))
 
 
 if __name__ == '__main__':
@@ -728,5 +887,7 @@ if __name__ == '__main__':
     # google_lenet()
     # print_resnet()
     # print_dense_net()
-    print_net_shape(ResNet())
+    # print_net_shape(ResNet())
     # print_net_shape(DenseNet(),size=96)
+    test_seq()
+    test_seq２()
--- deep/torch/lstm-learn.py
+++ deep/torch/lstm-learn.py
@@ -8,15 +8,80 @@ import sys
 
 sys.path.append("..")
 
-import zipfile
+import numpy as np
 
 import torch
+from torch import nn
+
+import deep.torch.d2lzh_pytorch as d2l
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 from util.logger_utils import get_log
 
 log = get_log("{}.log".format("lstm-learn"))
+
+(corpus_indices, char_to_idx, idx_to_char, vocab_size) = d2l.load_data_jay_lyrics()
+
+
+def test_one_hot():
+    x = torch.tensor([0, 2])
+    return d2l.one_hot(x, vocab_size)
+
+
+def test_to_onehot():
+    X = torch.arange(10).view(2, 5)
+    inputs = d2l.to_onehot(X, vocab_size)
+    print(len(inputs), inputs[0].shape)
+
+
+num_inputs, num_hiddens, num_outputs = vocab_size, 256, vocab_size
+print('will use', device)
+
+
+def get_params():
+    def _one(shape):
+        ts = torch.tensor(np.random.normal(0, 0.01,size=shape),device=device,dtype=torch.float32)
+        return torch.nn.Parameter(ts, requires_grad=True)
+
+    # 隐藏层参数
+    W_xh = _one((num_inputs, num_hiddens))
+    W_hh = _one((num_hiddens, num_hiddens))
+    b_h = torch.nn.Parameter(torch.zeros(num_hiddens, device=device, requires_grad=True))
+
+    # 输出层参数
+    W_hq = _one((num_hiddens, num_outputs))
+    b_q = torch.nn.Parameter(torch.zeros(num_outputs, device=device, requires_grad=True))
+    return nn.ParameterList([W_xh, W_hh, b_h, W_hq, b_q])
+
+
+def init_rnn_state(batch_size, num_hiddens, device):
+    return (torch.zeros((batch_size, num_hiddens), device=device),)
+
+
+def rnn(inputs, state, params):
+    # inputs和outputs皆为num_steps个形状为(batch_size, vocab_size)的矩阵
+    W_xh, W_hh, b_h, W_hq, b_q = params
+    H, = state
+    outputs = []
+    for X in inputs:
+        H = torch.tanh(torch.matmul(X, W_xh) + torch.matmul(H, W_hh) + b_h)
+        Y = torch.matmul(H, W_hq) + b_q
+        outputs.append(Y)
+    return outputs, (H,)
+
+
+def test_rnn():
+    X = torch.arange(10).view(2, 5)
+    state = init_rnn_state(X.shape[0],num_hiddens,device)
+    inputs = d2l.to_onehot(X.to(device),vocab_size)
+    params = get_params()
+    outputs,state_new = rnn(inputs,state,params)
+    print(len(outputs), outputs[0].shape, state_new[0].shape)
+
+    res = d2l.predict_rnn('分开', 10, rnn, params, init_rnn_state, num_hiddens, vocab_size,
+                device, idx_to_char, char_to_idx)
+    print(res)
 
 
 if __name__ == '__main__':
@@ -25,4 +90,12 @@ if __name__ == '__main__':
 
     # res = index_jay()
     # log.info("{}".format(res))
+
+    # res = test_one_hot()
+    # log.info("{}".format(res))
+    #
+    # test_to_onehot()
+    test_rnn()
+
+
     pass
