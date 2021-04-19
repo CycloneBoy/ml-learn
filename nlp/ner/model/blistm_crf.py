5cc0e65eaf95d334c44a9e3291266aaa55b766bf
cycloneboy <xuanfeng1992@gmail.com>
2021-04-19 23:10:10 +0800

    添加CRF模型和装饰者部分代码

--- nlp/ner/model/blistm_crf.py
+++ nlp/ner/model/blistm_crf.py
@@ -13,10 +13,11 @@ from itertools import zip_longest
 
 import torch
 import torch.nn as nn
+import torch.optim as optim
 
 from .bilstm import BiLSTM
-from .config import LSTMConfig
-from .utils import cal_loss
+from .config import LSTMConfig, TrainingConfig
+from .utils import cal_loss, cal_lstm_crf_loss, sort_by_lengths
 
 
 class BiLSTM_Model(object):
@@ -40,7 +41,60 @@ class BiLSTM_Model(object):
                                 self.hidden_size, out_size).to(self.device)
             self.calc_loss_func = cal_loss
         else:
-            pass
+            self.model = BiLSTM_CRF(vocab_size, self.emb_size,
+                                    self.hidden_size, out_size).to(self.device)
+            self.calc_loss_func = cal_lstm_crf_loss
+
+        # 加载训练参数：
+        self.epoches = TrainingConfig.epoches
+        self.print_step = TrainingConfig.print_step
+        self.lr = TrainingConfig.lr
+        self.batch_size = TrainingConfig.batch_size
+
+        # 初始化优化器
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
+
+        # 初始化其他指标
+        self.step = 0
+        self._best_val_loss = 1e18
+        self.best_model = None
+
+    def train(self, word_lists, tag_lists, dev_word_lists, dev_tag_lists, word2id, tag2id):
+        # 对数据集按照长度进行排序
+        word_lists, tag_lists, _ = sort_by_lengths(word_lists, tag_lists)
+        dev_word_lists, dev_tag_lists, _ = sort_by_lengths(dev_word_lists, dev_tag_lists)
+
+        B = self.batch_size
+        for e in range(1,self.epoches+1):
+            self.step = 0
+            losses = 0.
+            for ind in range(0,len(word_lists),B):
+                batch_sents = word_lists[ind,ind+B]
+                batch_tags = tag_lists[ind,ind+B]
+
+                losses += self.train_step(batch_sents,batch_tags,word2id,tag2id)
+                if self.step % TrainingConfig.print_step == 0:
+                    total_step = (len(word_lists) // B +1)
+                    print("Epoch {}, step/total_step: {}/{} {:.2f}% Loss:{:.4f}".format(
+                        e, self.step, total_step,
+                        100. * self.step / total_step,
+                        losses / self.print_step
+                    ))
+                    losses = 0.
+
+            # 每轮结束测试在验证集上的性能，保存最好的一个
+            val_loss = self.validate(
+                dev_word_lists,dev_tag_lists,word2id,tag2id)
+            print("Epoch {}, Val Loss:{:.4f}".format(e, val_loss))
+
+    def train_step(self, batch_sents, batch_tags, word2id, tag2id):
+        self.model.train()
+        self.step +=1
+        # 准备数据
+        tensorized_sents ,lengths = tensorized(batch_sents,word2id)
+
+    def validate(self, dev_word_lists, dev_tag_lists, word2id, tag2id):
+        pass
 
 
 class BiLSTM_CRF(nn.Module):
@@ -92,31 +146,31 @@ class BiLSTM_CRF(nn.Module):
         # viterbi[i, j, k]表示第i个句子，第j个字对应第k个标记的最大分数
         viterbi = torch.zeros(B, L, T).to(device)
         # backpointer[i, j, k]表示第i个句子，第j个字对应第k个标记时前一个标记的id，用于回溯
-        backpointer = (torch.zeros(B,L,T).long() * end_id).to(device)
-        lengths =torch.LongTensor(lengths).to(device)
+        backpointer = (torch.zeros(B, L, T).long() * end_id).to(device)
+        lengths = torch.LongTensor(lengths).to(device)
         # 向前递推
         for step in range(L):
             batch_size_t = (lengths > step).sum().item()
             if step == 0:
                 # 第一个字它的前一个标记只能是start_id
-                viterbi[:batch_size_t,step,:] =crf_scores[:batch_size_t,step,start_id,:]
-                backpointer[:batch_size_t,step,:] = start_id
+                viterbi[:batch_size_t, step, :] = crf_scores[:batch_size_t, step, start_id, :]
+                backpointer[:batch_size_t, step, :] = start_id
             else:
-                max_scores,prev_tags =torch.max(
-                    viterbi[:batch_size_t,step -1,:].unsqueeze(2) +
-                    crf_scores[:batch_size_t,step,:,:] ,   # [B, T, T]
+                max_scores, prev_tags = torch.max(
+                    viterbi[:batch_size_t, step - 1, :].unsqueeze(2) +
+                    crf_scores[:batch_size_t, step, :, :],  # [B, T, T]
                     dim=1
                 )
-                viterbi[:batch_size_t,step,:] = max_scores
-                backpointer[:batch_size_t,step,:] = prev_tags
+                viterbi[:batch_size_t, step, :] = max_scores
+                backpointer[:batch_size_t, step, :] = prev_tags
 
         # 在回溯的时候我们只需要用到backpointer矩阵
-        backpointer = backpointer.view(B,-1)   # [B, L * T]
-        tagids = [] # 存放结果
+        backpointer = backpointer.view(B, -1)  # [B, L * T]
+        tagids = []  # 存放结果
         tags_t = None
-        for step in range(L-1,0,-1):
+        for step in range(L - 1, 0, -1):
             batch_size_t = (lengths > step).sum().item()
-            if step == L -1:
+            if step == L - 1:
                 index = torch.ones(batch_size_t).long() * (step * tagset_size)
                 index = index.to(device)
                 index += end_id
@@ -124,18 +178,18 @@ class BiLSTM_CRF(nn.Module):
                 prev_batch_size_t = len(tags_t)
 
                 new_in_batch = torch.LongTensor(
-                    [end_id] *(batch_size_t - prev_batch_size_t))
+                    [end_id] * (batch_size_t - prev_batch_size_t))
                 offset = torch.cat(
-                    [tags_t,new_in_batch],
+                    [tags_t, new_in_batch],
                     dim=0
-                ) # 这个offset实际上就是前一时刻的
+                )  # 这个offset实际上就是前一时刻的
                 index = torch.ones(batch_size_t).long() * (step * tagset_size)
                 index = index.to(device)
                 index += offset.long()
 
             try:
                 tags_t = backpointer[:batch_size_t].gather(
-                    dim=1,index=index.unsqueeze(1).long())
+                    dim=1, index=index.unsqueeze(1).long())
             except RuntimeError:
                 import pdb
                 pdb.set_trace()
@@ -144,9 +198,8 @@ class BiLSTM_CRF(nn.Module):
         # tagids:[L-1]（L-1是因为扣去了end_token),大小的liebiao
         # 其中列表内的元素是该batch在该时刻的标记
         # 下面修正其顺序，并将维度转换为 [B, L]
-        tagids = list(zip_longest(*reversed(tagids),fillvalue=pad))
+        tagids = list(zip_longest(*reversed(tagids), fillvalue=pad))
         tagids = torch.Tensor(tagids).long()
 
         # 返回解码的结果
         return tagids
-
